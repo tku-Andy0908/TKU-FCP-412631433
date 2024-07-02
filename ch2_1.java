d728485aafa645410dd16f0d46706c705bcf0206
tku-Andy0908 <412631433@o365.tku.edu.tw>
2024-07-02 11:55:29 +0800

    Add files via upload

---
+++ ch2_1.java
@@ -0,0 +1,12 @@
+public class ch2_1 {  //定義public類別ch2_1
+    public static void main(String[] args) { //main()函數，主程式開始
+        int num1=2; //宣告整數num1
+        int num2=3; //宣告整數num2
+        float num3=4;
+        String num4="五";
+        System.out.println(num1+"cats are running"); //印出字串及變數內容
+        System.out.println(num2+"dogs are running"); //印出字串及變數內容
+        System.out.println(num3+"dogs are running"); //印出字串及變數內容
+        System.out.println(num4+"dogs are running"); //印出字串及變數內容
+    }
+}
