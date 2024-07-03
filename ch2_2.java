c89cb9e245466f1e7285706b052209542f13b6da
tku-Andy0908 <412631433@o365.tku.edu.tw>
2024-07-03 11:02:53 +0800

    Add files via upload

---
+++ ch2_2.java
@@ -0,0 +1,13 @@
+//ch2_2,變數的宣告
+public class ch2_2 {        //定義類別ch2_2           
+
+   public static void main(String[] args) {
+    int a;  //宣告變數a,但不設定初值
+    int b=4,sum; //宣告變數b和sum,但只將b設為4
+
+    a=5;         //設定a值為5
+    sum=a+b;     //將sum值設為a+b
+    System.out.println("sum="+sum); //印出sum值
+   }
+
+   }
