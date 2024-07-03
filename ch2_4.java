c12e1e4bf5fb2b0d3b8713150a85f6e09077c718
tku-Andy0908 <412631433@o365.tku.edu.tw>
2024-07-03 11:45:46 +0800

    Add files via upload

---
+++ ch2_4.java
@@ -0,0 +1,10 @@
+//ch2_4有錯誤得程式
+public class ch2_4 {
+    public static void main(String[] args) {
+        int num1=2; //宣告整數變數num1,並設置為2
+        int num2=3; //宣告變數整數num2,並設置為3
+
+        System.out.println("I have "+ num1 +" dogs");
+        System.out.println("You have "+ num2 +" dogs");
+    }
+}
---
+++ ch2_5.java
@@ -0,0 +1,10 @@
+//ch2_5語意錯誤的程式
+public class ch2_5 {
+    public static void main(String[] args) {
+        int a=2; //宣告變數a,並設置為2
+        int b=3; //宣告變數b,並設置為3
+        int c=a+b; //宣告變數c,設置為a+b
+        System.out.println("a+b="+c); 
+    }
+    
+}
